a16fbe34e1b13c6a38bd385e5f8e166cfaa01012
Raman Temre <ramantemre2110@gmail.com>
2025-07-17 18:57:57 +0530

    implement fibonacci series logic

--- questions/fibonacci.js
+++ questions/fibonacci.js
@@ -5,4 +5,14 @@ function fibonacci(n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(5));
+console.log(fibonacci(5)); // 5
+
+var fibonacciSeries = function (n) {
+  const arr = [0, 1];
+  for (let i = 2; i < n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr;
+};
+
+console.log(fibonacciSeries(5)); // [0, 1, 1, 2, 3] 
