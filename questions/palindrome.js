8c9148d50837602d1c71a43f874755526a078095
Raman Temre <ramantemre2110@gmail.com>
2025-08-02 10:57:50 +0530

    Palindrome linked list

--- questions/palindrome.js
+++ questions/palindrome.js
@@ -12,3 +12,30 @@ const res = isPalindrome(10);
 console.log(res);
 
 // 121 => "121" => ["1","2","1"] => ["1","2","1"] => "121"
+
+// =============================================================================
+
+// Ques 1 : Given the head of a singly linked list, return true if it is
+// a palindrome or false otherwise
+
+// Input: head = [1,2,2,1]      ----->>>>>      Output: true;
+// Input: head = [1,2]          ----->>>>>      Output: false;
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+var isPalindromeLinkedList = function (head) {
+  let string1 = (string2 = "");
+  let node = head;
+
+  while (node != null) {
+    string1 = `${string1}${node.val}`;
+    string2 = `${node.val}${string2}`;
+    node = node.next;
+  }
+  return string1 === string2;
+};
